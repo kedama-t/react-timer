1240475c4b50b163a87d78cb96d9ea655014fac5
kedama-t <mt.masaaki.takeda+github@gmail.com>
2024-04-06 02:58:06 +0900

    カウントアップに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { ReactEventHandler, useEffect, useState } from 'react';
-import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 import workerInterval from './lib/workerIntervals';
 import 'react-circular-progressbar/dist/styles.css';
 
 const App = () => {
   const [time, setTime] = useState({ minutes: 0, seconds: 0 });
+  const [goal, setGoal] = useState(90);
   const [isActive, setIsActive] = useState(false);
   const handleStart = (event) => {
     event.preventDefault();
-    setTime({ minutes: Number(event.target[0].value), seconds: 0 });
+    setGoal(Number(event.target[0].value));
+    setTime({ minutes: 0, seconds: 0 });
     setIsActive(true);
   };
   const handleStop: ReactEventHandler = (event) => {
@@ -21,14 +23,10 @@ const App = () => {
       return;
     }
     const id = workerInterval.setInterval(() => {
-      if (time.seconds === 0) {
-        if (time.minutes === 0) {
-          setIsActive(false);
-        } else {
-          setTime({ minutes: time.minutes - 1, seconds: 59 });
-        }
+      if (time.seconds === 59) {
+        setTime({ minutes: time.minutes + 1, seconds: 0 });
       } else {
-        setTime({ minutes: time.minutes, seconds: time.seconds - 1 });
+        setTime({ minutes: time.minutes, seconds: time.seconds + 1 });
       }
     }, 1000);
     return () => workerInterval.clearInterval(id);
@@ -39,14 +37,23 @@ const App = () => {
       {isActive ? (
         <div className="flex flex-col gap-2 items-center">
           <CircularProgressbarWithChildren
+            background
+            backgroundPadding={6}
+            styles={buildStyles({
+              strokeLinecap: "butt",
+              backgroundColor: "rgb(253 230 138)",
+              textColor: "rgb(217 119 6)",
+              pathColor: "rgb(217 119 6)",
+              trailColor: "transparent"
+            })}
             value={time.seconds}
             maxValue={60}
           >
-            <p>
-              <span className="text-8xl">{time.minutes.toString()}</span>min
+            <p className="text-amber-600">
+              <span className="text-8xl">{time.minutes.toString()}</span>/ {goal} min
             </p>
           </CircularProgressbarWithChildren>
-          <button className="bg-slate-200" onClick={handleStop}>
+          <button className="bg-amber-200" onClick={handleStop}>
             Stop
           </button>
         </div>
@@ -59,12 +66,12 @@ const App = () => {
           <CircularProgressbarWithChildren
             value={0}
             maxValue={60}
-            >
+          >
             <input
               className="w-40 h-40 text-center text-8xl inline-block"
               type="number"
               name="time"
-              defaultValue={90}
+              defaultValue={goal}
             />
           </CircularProgressbarWithChildren>
           <button className="bg-slate-200" type="submit">
