21f0fe1aefdb2607b8d61e57ec6e80e4df5033a2
kedama-t <mt.masaaki.takeda+github@gmail.com>
2024-04-06 11:45:27 +0900

    スタイリングを調整

--- src/App.tsx
+++ src/App.tsx
@@ -32,12 +32,25 @@ const App = () => {
     return () => workerInterval.clearInterval(id);
   }, [time, isActive]);
 
+  const { circularStyle, textStyle, bgStyle } = time.minutes < goal
+    ? {
+      circularStyle: "[&_.CircularProgressbar-path]:stroke-amber-300 [&_.CircularProgressbar-background]:fill-amber-600",
+      textStyle: "text-amber-300",
+      bgStyle: "bg-amber-300"
+    }
+    : {
+      circularStyle: "[&_.CircularProgressbar-path]:stroke-teal-300 [&_.CircularProgressbar-background]:fill-teal-600",
+      textStyle: "text-teal-300",
+      bgStyle: "bg-teal-300"
+    }
+
   return (
-    <>
+    <div className="p-4">
       {isActive ? (
         <div className="flex flex-col gap-2 items-center">
+        <h2>読書中…</h2>
           <CircularProgressbarWithChildren
-          className="[&_.CircularProgressbar-path]:stroke-amber-300 [&_.CircularProgressbar-background]:fill-amber-600"
+            className={circularStyle}
             background
             backgroundPadding={8}
             styles={buildStyles({
@@ -47,11 +60,11 @@ const App = () => {
             value={time.seconds}
             maxValue={60}
           >
-            <p className="text-amber-300">
-              <span className="text-8xl">{time.minutes.toString()}</span>/ {goal} 分
+            <p className={textStyle}>
+              <span className="text-7xl">{time.minutes.toString()}</span>/ {goal} 分
             </p>
           </CircularProgressbarWithChildren>
-          <button className="bg-amber-300" onClick={handleStop}>
+          <button className={`p-4 rounded-xl ${bgStyle}`} onClick={handleStop}>
             Stop
           </button>
         </div>
@@ -60,24 +73,27 @@ const App = () => {
           className="flex flex-col gap-2 items-center"
           onSubmit={handleStart}
         >
-
+        <h2>目標時間を設定</h2>
           <CircularProgressbarWithChildren
             value={0}
             maxValue={60}
           >
-            <input
-              className="w-40 h-40 text-center text-8xl inline-block"
-              type="number"
-              name="time"
-              defaultValue={goal}
-            />
+            <div className="flex flex-row gap-2 items-baseline">
+              <input
+                className="w-36 h-24 text-right text-7xl"
+                type="number"
+                name="time"
+                id="time"
+                defaultValue={goal}
+              /><label htmlFor="time">分</label>
+            </div>
           </CircularProgressbarWithChildren>
-          <button className="bg-slate-200" type="submit">
+          <button className="p-4 rounded-xl bg-slate-200" type="submit">
             Start
           </button>
         </form>
       )}
-    </>
+    </div>
   );
 };
 
